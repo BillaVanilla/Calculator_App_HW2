9e31afe7b491e2205bdc4cc9321a084d9209db3b
BillaVanilla <bfofung@gmail.com>
2024-10-03 16:05:23 -0400

    removed button press for counter

--- lib/main.dart
+++ lib/main.dart
@@ -127,26 +127,10 @@ Widget Buttonbuilder(String text, Function() onPressed, {Color? color}) {
         title: Text(widget.title),
       ),
       body: Center(
-        
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'none',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
